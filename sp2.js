821d9dcf52171f625d6fb12862acfd2bf64b9b86
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-05-28 19:16:46 +0530

    Create sp2.js

---
+++ sp2.js
@@ -0,0 +1,13 @@
+var arrayStringsAreEqual = function(word1, word2) {
+    let c1,c2;
+    c1=word1.join("")
+    c2=word2.join("")
+    if(c1==c2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
